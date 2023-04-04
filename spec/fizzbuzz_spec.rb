fadf4958160be6a3c21c959ce4e8b0982ea7cb52
marco <nfk18@hotmail.co.uk>
2023-04-04 11:15:43 +0000

    using loops in spec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,22 +2,49 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'should return "fizz" when argument is divisible by 3' do
-    expect(3.fizzbuzz).to eq "fizz"
-    expect(9.fizzbuzz).to eq "fizz"
+    i =3
+    while i <= 100 
+      
+      if i % 5 == 0 
+        i += 3
+      else
+        expect(i.fizzbuzz).to eq "fizz"
+        i += 3
+      end
+    end
   end
   
   it 'should return "buzz" when argument is divisible by 5' do
-    expect(5.fizzbuzz).to eq "buzz" 
-    expect(10.fizzbuzz).to eq "buzz" 
+    i =5
+    while i <= 100 
+      
+      if i % 3 == 0 
+        i += 5
+      else
+        expect(i.fizzbuzz).to eq "buzz"
+        i += 5
+      end
+    end
   end
   
   it 'should return "fizzbuzz" when argument is divisible by 5 and 3' do
-    expect(15.fizzbuzz).to eq "fizzbuzz"
-    expect(30.fizzbuzz).to eq "fizzbuzz"
+    i =15
+    while i <= 100 
+        expect(i.fizzbuzz).to eq "fizzbuzz"
+        i += 15
+    end
   end
   
   it 'should return itself as a string when argument is a number not divisible by either 3 or 5' do
-    expect(7.fizzbuzz).to eq "7"  
-    expect(13.fizzbuzz).to eq "13"  
+       i =1
+    while i <= 100 
+      
+      if i % 5 == 0 || i % 3 == 0
+        i += 1
+      else
+        expect(i.fizzbuzz).to eq "#{i}"
+        i += 1
+      end
+    end  
   end 
 end
