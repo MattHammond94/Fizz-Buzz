867c51cf4c4a610f19a4da7de74278f4b14a5fd5
marco <nfk18@hotmail.co.uk>
2023-04-04 10:24:22 +0000

    passed basic tests 2

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(number)
-  if number == 15
+  if number % 3 == 0 && number % 5 == 0
     "fizzbuzz"
-  elsif number == 5
+  elsif number % 5 == 0
     "buzz"
-  elsif number == 3
+  elsif number % 3 == 0
     "fizz"
+  else 
+    number.to_s
   end
 end
