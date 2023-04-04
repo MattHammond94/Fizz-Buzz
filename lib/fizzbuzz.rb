c9850cdd0f6047f6774ab24edee21b28ff0df94b
MattyMooMilk <mjhammond94@gmail.com>
2023-04-04 10:16:43 +0000

    basic tests passed

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,9 @@
+def fizzbuzz(number)
+  if number == 15
+    "fizzbuzz"
+  elsif number == 5
+    "buzz"
+  elsif number == 3
+    "fizz"
+  end
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,15 @@
+require 'fizzbuzz'
+
 describe 'fizzbuzz' do
   it 'should return "fizz" when input is 3' do
     expect(fizzbuzz(3)).to eq "fizz"
+  end
+  
+  it 'should return "buzz" when input is 5' do
+    expect(fizzbuzz(5)).to eq "buzz" 
+  end
+  
+  it 'Should return "fizzbuzz" when input is 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
 end
